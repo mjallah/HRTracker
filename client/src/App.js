7c229273c271ea93f22a2ca77b2e88d1d20baeab
Malik Jallah <malikjallah@gmail.com>
2021-01-03 13:12:03 -0600

    Added some css and added add employee button

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Hello!</div>;
+  return (
+    <div className="App">
+      <div className="information">
+        <label>Name:</label>
+        <input type="text" />
+        <label>Position:</label>
+        <input type="text" />
+        <label>Salary:</label>
+        <input type="number" />
+        <label>Manager:</label>
+        <input type="text" />
+        <button>Add Employee</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
