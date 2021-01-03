cacb8ffcf4d5082da2c8601a41e77ad80fa93f56
Malik Jallah <malikjallah@gmail.com>
2021-01-03 13:34:44 -0600

    added const

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,47 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [id, setID] = useState(0)
+  const [name, setName] = useState("")
+  const [position, setPosition] = useState("")
+  const [salary, setSalary] = useState(0)
+  const [manager, setManager] = useState("")
+
   return (
     <div className="App">
       <div className="information">
+        <label>ID:</label>
+        <input type="number"
+          onChange={(event) => {
+            setID(event.target.value);
+          }}
+        />
         <label>Name:</label>
-        <input type="text" />
+        <input type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
         <label>Position:</label>
-        <input type="text" />
+        <input type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        />
         <label>Salary:</label>
-        <input type="number" />
+        <input type="number"
+          onChange={(event) => {
+            setSalary(event.target.value);
+          }}
+        />
         <label>Manager:</label>
-        <input type="text" />
+        <input type="text"
+          onChange={(event) => {
+            setManager(event.target.value);
+          }}
+        />
         <button>Add Employee</button>
       </div>
     </div>
